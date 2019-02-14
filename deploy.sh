e3b6c7be7d030e25d9b1926dbcd3721c691e6b57
kelj0 <kegljevickarlo@gmail.com>
2019-02-14 22:15:45 +0100

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
-echo "
-==========================
+echo "Running kelj0 deploy script
+-------------------------------------
+| x  x xxx x     x  xxxx       xxxx |
+| x x  x   x     x  x  x       x  x |
+| xx   xxx x     x  x  x  x x  x  x |
+| x x  x   x   x x  x  x   x   x  x |
+| x  x xxx xxx x_x  xxxx  x x  xxxx |
+-------------------------------------
+"
+
+echo "==========================
 ||Creating Github folder||
 ==========================
 "
+
 mkdir /home/$USER/github
 echo done
 
-echo "
-==========================
+echo "==========================
 ||    Cloning repos     ||
 ==========================
 "
+
 clone="git clone https://github.com/kelj0/"
 cd /home/$USER/github
 $clone"LearningPython.git" \
@@ -24,12 +34,12 @@ $clone"LearningPython.git" \
 && $clone"TestingArea.git"
 echo done
 
-echo "
-==========================
-|| Cloning TestingArea  ||
+echo "==========================
+||    cp TestingArea    ||
 ==========================
 "
-cd ../ && $clone"TestingArea.git"
+
+cp -r /home/$USER/github/TestingArea ../
 echo done
 
 echo "TestingArea -> Testing"
@@ -40,11 +50,37 @@ echo "rm .git from Testing"
 sudo rm -rf /home/$USER/Testing/.git
 echo done
 
-echo "
-==========================
-||    Adding aliases    ||
+echo "==========================
+||     Adding bashrc    ||
 ==========================
 "
-wget -O - https://pastebin.com/raw/tjH1gndT >> ~/.bashrc && . ~/.bashrc
+
+sudo cp /home/$USER/github/dotfiles/.bashrc ~/
 echo done
-echo "Dont forget to append xfiles to bashrc! (https://pastebin.com/raw/h1SPeRCr)"
+
+
+
+
+echo "
+==========================
+||     neovim stuff     ||
+==========================
+"
+cd ~
+echo "download and install appimage"
+wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim # download and install appimage
+echo "make nvim accessible to all users"
+chmod +x nvim # make nvim accessible to all users
+sudo chown root:root nvim
+echo "move bin file"
+sudo mv nvim /usr/bin # move bin file 
+mkdir -p ~/.config/nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # vim plugin manager
+echo "copy configs"
+sudo cp  /home/$USER/github/dotfiles/nvim/init.vim ~/.config/nvim # copy configs
+echo "pip install neovim"
+pip3 install --user neovim
+pip install --user neovim
+
+echo done
+
